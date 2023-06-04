c0254a93c415bb4e1d1e275d44111af15c632fba
Actinolight <128472691+LemonKINGActinolight@users.noreply.github.com>
2023-06-04 22:14:44 +0900

    English、日本語対応 add sulfur!!

--- src/main/java/com/ShirakawaMIN/ShirakawaMod/main/item/Itemsulfur.java
+++ src/main/java/com/ShirakawaMIN/ShirakawaMod/main/item/Itemsulfur.java
@@ -1,11 +1,14 @@
 package com.ShirakawaMIN.ShirakawaMod.main.item;
 
+import com.ShirakawaMIN.ShirakawaMod.main.ShirakawaMod;
+import com.ShirakawaMIN.ShirakawaMod.main.ShirakawaModtabu;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 
 public class Itemsulfur extends Item {
+
     public Itemsulfur() {
-        super(new Properties().tab(CreativeModeTab.TAB_SEARCH));
+        super(new Properties().tab(ShirakawaMod.SHIRAKAWAMOD_TAB));
         this.setRegistryName("sulfur");
     }
 }
--- src/main/java/com/ShirakawaMIN/ShirakawaMod/main/regi/ShirakawaModItems.java
+++ src/main/java/com/ShirakawaMIN/ShirakawaMod/main/regi/ShirakawaModItems.java
@@ -3,27 +3,24 @@ package com.ShirakawaMIN.ShirakawaMod.main.regi;
 import com.ShirakawaMIN.ShirakawaMod.main.ShirakawaMod;
 import com.ShirakawaMIN.ShirakawaMod.main.item.Itemsulfur;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder(ShirakawaMod.MOD_ID)
-public class ShirakawaMod_items {
+public class ShirakawaModItems {
 
-public static final Itemsulfur ITEMSULFUR = new Itemsulfur();
+    public static final Itemsulfur ITEMSULFUR = new Itemsulfur();
+
     @Mod.EventBusSubscriber(modid = ShirakawaMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class register{
+    public static class Register {
 
         @SubscribeEvent
-        public static void registerItems(final RegistryEvent.Register<Item> event){
-
-            final Item[] items = {
+        public static void registerItems(final RegistryEvent.Register<Item> event) {
+            event.getRegistry().registerAll(
                     ITEMSULFUR
-            };
-            event.getRegistry().registerAll(Items);
+            );
         }
     }
-
 }
