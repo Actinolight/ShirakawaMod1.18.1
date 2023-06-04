423aad8d585fb101f56aff589168949919d40856
Actinolight <128472691+LemonKINGActinolight@users.noreply.github.com>
2023-06-04 20:50:36 +0900

    itemtabの追加 itemtabの名前追加

--- src/main/java/com/ShirakawaMIN/ShirakawaMod/main/ShirakawaMod.java
+++ src/main/java/com/ShirakawaMIN/ShirakawaMod/main/ShirakawaMod.java
@@ -1,9 +1,12 @@
 package com.ShirakawaMIN.ShirakawaMod.main;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("shirakawamod")
 public class ShirakawaMod {
 
     public static final String MOD_ID = "shirakawamod";
+
+    public static final CreativeModeTab SHIRAKAWAMOD_TAB = new ShirakawaModtabu();
 }
