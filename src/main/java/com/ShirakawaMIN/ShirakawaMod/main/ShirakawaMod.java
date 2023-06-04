2aa87cd5a5c40494a09e2a11cd38ae141fe5f4ac
Actinolight <128472691+LemonKINGActinolight@users.noreply.github.com>
2023-06-04 19:32:17 +0900

    Initial commit

---
+++ src/main/java/com/ShirakawaMIN/ShirakawaMod/main/ShirakawaMod.java
@@ -0,0 +1,9 @@
+package com.ShirakawaMIN.ShirakawaMod.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("shirakawamod")
+public class ShirakawaMod {
+
+    public static final String MOD_ID = "shirakawamod";
+}
